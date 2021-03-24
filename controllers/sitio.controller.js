e9836851fcda8685f80c9bbb2ddfea74be3c0d1a
Francisco Cunha <43752840+a84059@users.noreply.github.com>
2021-03-24 10:56:50 +0000

    sitiohidrografia

--- controllers/sitio.controller.js
+++ controllers/sitio.controller.js
@@ -5,7 +5,7 @@ const jsonMessages = require(jsonMessagesPath + 'bd')
 
 var exports = module.exports = {};
 
-
+//sitio
 exports.sitio = function (req, res) {
    const query = connect.con.query("SELECT * FROM sitio", function(error, rows, fields){
         console.log(query.sql);
@@ -21,11 +21,11 @@ exports.sitio = function (req, res) {
                 res.send(rows);
                 }
             }
- 
     });
-
 }
 
+
+//sitioId
 exports.sitioId = function (req, res) {
     const query = connect.con.query("SELECT * FROM sitio WHERE id_sitio = ?",  [req.params.id_sitio], function(error, rows, fields){
          console.log(query.sql);
@@ -41,7 +41,25 @@ exports.sitioId = function (req, res) {
                  res.send(rows);
                  }
              }
-  
      });
- 
+ }
+
+
+ //sitioHidrografia
+ exports.sitioHidrografia = function (req, res) {
+    const query = connect.con.query("SELECT * FROM sitio_hidrografia WHERE id_sitio = ?",  [req.params.id_sitio], function(error, rows, fields){
+         console.log(query.sql);
+     if (error) {
+             console.log(error);
+             res.status(jsonMessages.db.dbError.status).send(jsonMessages.db.dbError);
+             }
+             else {
+                 if(rows.length == 0){              
+                 res.status(jsonMessages.db.noRecords.status).send(jsonMessages.db.noRecords);  
+                 }
+                 else{
+                 res.send(rows);
+                 }
+             }
+     });
  }
--- routes/sitio.route.js
+++ routes/sitio.route.js
@@ -2,7 +2,15 @@
 const getController = require('../controllers/sitio.controller.js');
 
 module.exports = function(app) {
-    //sitio Routes
+    // Routes utilizadas para fazer os pedidos GET de tudo o que esteja relacionado com o Sitio
+
+    //Devolve todos os sitios existentes
     app.get('/sitio', getController.sitio); 
-    app.get('/sitio/:id_sitio', getController.sitioId);    
+
+    //Devolve um sitio consoante o seu ID
+    app.get('/sitio/:id_sitio', getController.sitioId);
+
+    //Devolve um sitiohidrografia consoante o ID do sitio  
+    app.get('/sitioHidrografia/:id_sitio', getController.sitioHidrografia);
+
 };
