0209524e655a96e2d75685c6b4b05259a463c002
Francisco Cunha <43752840+a84059@users.noreply.github.com>
2021-03-25 14:13:34 +0000

    Contexto / Hidrografia

--- controllers/sitio.controller.js
+++ controllers/sitio.controller.js
@@ -6,66 +6,174 @@ const jsonMessages = require(jsonMessagesPath + 'bd')
 var exports = module.exports = {};
 
 
-
+/*-------------------------------Sitio-------------------------------*/
 
 //sitio
 exports.sitio = function (req, res) {
-    const query = connect.con.query("SELECT * FROM sitio", function(error, rows, fields){
+    const query = connect.con.query("SELECT * FROM sitio", function (error, rows, fields) {
         console.log(query.sql);
-    if (error) {
+        if (error) {
             console.log(error);
             res.status(jsonMessages.db.dbError.status).send(jsonMessages.db.dbError);
-            }
-            else {
-                if(rows.length == 0){              
-                res.status(jsonMessages.db.noRecords.status).send(jsonMessages.db.noRecords);  
-                }
-                else{
+        } else {
+            if (rows.length == 0) {
+                res.status(jsonMessages.db.noRecords.status).send(jsonMessages.db.noRecords);
+            } else {
                 res.send(rows);
-                }
             }
+        }
+    })
+
+}
+
+
+
+//sitioId
+exports.sitioId = function (req, res) {
+    const query = connect.con.query("SELECT * FROM sitio WHERE id_sitio = ?", [req.params.id_sitio], function (error, rows, fields) {
+        console.log(query.sql);
+        if (error) {
+            console.log(error);
+            res.status(jsonMessages.db.dbError.status).send(jsonMessages.db.dbError);
+        } else {
+            if (rows.length == 0) {
+                res.status(jsonMessages.db.noRecords.status).send(jsonMessages.db.noRecords);
+            } else {
+                res.send(rows);
+            }
+        }
     })
 
 }
 
 
 //sitioId
-exports.sitioId = function (req, res) {
-    const query = connect.con.query("SELECT * FROM sitio WHERE id_sitio = ?",  [req.params.id_sitio], function(error, rows, fields){
-         console.log(query.sql);
-     if (error) {
-             console.log(error);
-             res.status(jsonMessages.db.dbError.status).send(jsonMessages.db.dbError);
-             }
-             else {
-                 if(rows.length == 0){              
-                 res.status(jsonMessages.db.noRecords.status).send(jsonMessages.db.noRecords);  
-                 }
-                 else{
-                 res.send(rows);
-                 }
-             }
-     })
+exports.sitioCoord = function (req, res) {
+    const query = connect.con.query("SELECT * FROM sitio WHERE coord_X = ? AND coord_Y = ?", [req.params.coord_X, req.params.coord_Y], function (error, rows, fields) {
+        console.log(query.sql);
+        if (error) {
+            console.log(error);
+            res.status(jsonMessages.db.dbError.status).send(jsonMessages.db.dbError);
+        } else {
+            if (rows.length == 0) {
+                res.status(jsonMessages.db.noRecords.status).send(jsonMessages.db.noRecords);
+            } else {
+                res.send(rows);
+            }
+        }
+    })
 
- }
+}
 
 
- //sitioHidrografia
- exports.sitioHidrografia = function (req, res) {
-    const query =  connect.con.query("SELECT * FROM sitio_hidrografia WHERE id_sitio = ?",  [req.params.id_sitio], function(error, rows, fields){
-         console.log(query.sql);
-     if (error) {
-             console.log(error);
-             res.status(jsonMessages.db.dbError.status).send(jsonMessages.db.dbError);
-             }
-             else {
-                 if(rows.length == 0){              
-                 res.status(jsonMessages.db.noRecords.status).send(jsonMessages.db.noRecords);  
-                 }
-                 else{
-                 res.send(rows);
-                 }
-             }
-     })
 
- }
+/*-------------------------------HIDROGRAFIA-------------------------------*/
+
+//sitioHidrografia
+exports.sitioHidrografia = function (req, res) {
+    const query = connect.con.query("SELECT * FROM sitio_hidrografia", function (error, rows, fields) {
+        console.log(query.sql);
+        if (error) {
+            console.log(error);
+            res.status(jsonMessages.db.dbError.status).send(jsonMessages.db.dbError);
+        } else {
+            if (rows.length == 0) {
+                res.status(jsonMessages.db.noRecords.status).send(jsonMessages.db.noRecords);
+            } else {
+                res.send(rows);
+            }
+        }
+    })
+
+}
+
+//sitioHidrografiaID
+exports.sitioHidrografiaID = function (req, res) {
+    const query = connect.con.query("SELECT * FROM sitio_hidrografia WHERE id = ?", [req.params.id], function (error, rows, fields) {
+        console.log(query.sql);
+        if (error) {
+            console.log(error);
+            res.status(jsonMessages.db.dbError.status).send(jsonMessages.db.dbError);
+        } else {
+            if (rows.length == 0) {
+                res.status(jsonMessages.db.noRecords.status).send(jsonMessages.db.noRecords);
+            } else {
+                res.send(rows);
+            }
+        }
+    })
+
+}
+
+//sitioHidrografiasitioID
+exports.sitioHidrografiaIDsitio = function (req, res) {
+    const query = connect.con.query("SELECT * FROM sitio_hidrografia WHERE id_sitio = ?", [req.params.id_sitio], function (error, rows, fields) {
+        console.log(query.sql);
+        if (error) {
+            console.log(error);
+            res.status(jsonMessages.db.dbError.status).send(jsonMessages.db.dbError);
+        } else {
+            if (rows.length == 0) {
+                res.status(jsonMessages.db.noRecords.status).send(jsonMessages.db.noRecords);
+            } else {
+                res.send(rows);
+            }
+        }
+    })
+
+}
+
+
+/*-------------------------------Contexto-------------------------------*/
+
+exports.sitioContexto = function (req, res) {
+    const query = connect.con.query("SELECT * FROM sitio_contexto_geog", function (error, rows, fields) {
+        console.log(query.sql);
+        if (error) {
+            console.log(error);
+            res.status(jsonMessages.db.dbError.status).send(jsonMessages.db.dbError);
+        } else {
+            if (rows.length == 0) {
+                res.status(jsonMessages.db.noRecords.status).send(jsonMessages.db.noRecords);
+            } else {
+                res.send(rows);
+            }
+        }
+    })
+}
+
+//sitioContextoID
+exports.sitioContextoID = function (req, res) {
+    const query = connect.con.query("SELECT * FROM sitio_contexto_geog WHERE id = ?", [req.params.id], function (error, rows, fields) {
+        console.log(query.sql);
+        if (error) {
+            console.log(error);
+            res.status(jsonMessages.db.dbError.status).send(jsonMessages.db.dbError);
+        } else {
+            if (rows.length == 0) {
+                res.status(jsonMessages.db.noRecords.status).send(jsonMessages.db.noRecords);
+            } else {
+                res.send(rows);
+            }
+        }
+    })
+
+}
+
+//sitioContextoIDsitio
+exports.sitioContextoIDsitio = function (req, res) {
+    const query = connect.con.query("SELECT * FROM sitio_contexto_geog WHERE id_sitio = ?", [req.params.id_sitio], function (error, rows, fields) {
+        console.log(query.sql);
+        if (error) {
+            console.log(error);
+            res.status(jsonMessages.db.dbError.status).send(jsonMessages.db.dbError);
+        } else {
+            if (rows.length == 0) {
+                res.status(jsonMessages.db.noRecords.status).send(jsonMessages.db.noRecords);
+            } else {
+                res.send(rows);
+            }
+        }
+    })
+
+}
--- routes/sitio.route.js
+++ routes/sitio.route.js
@@ -4,13 +4,38 @@ const getController = require('../controllers/sitio.controller.js');
 module.exports = function(app) {
     // Routes utilizadas para fazer os pedidos GET de tudo o que esteja relacionado com o Sitio
 
+
+    /*-------------------------------Sitio-------------------------------*/
     //Devolve todos os sitios existentes
     app.get('/sitio', getController.sitio); 
 
     //Devolve um sitio consoante o seu ID
     app.get('/sitio/:id_sitio', getController.sitioId);
 
+    //Devolve um sitio consoante as coordenadas X e Y
+    app.get('/sitio/:coord_X/:coord_Y', getController.sitioCoord);
+
+
+    /*-------------------------------Hidrografia-------------------------------*/
     //Devolve um sitiohidrografia consoante o ID do sitio  
-    app.get('/sitioHidrografia/:id_sitio', getController.sitioHidrografia);
+    app.get('/sitioHidrografia/', getController.sitioHidrografia);
+
+    //Devolve um sitiohidrografia consoante o ID 
+    app.get('/sitioHidrografia/:id', getController.sitioHidrografiaID);
+
+    //Devolve um sitiohidrografia consoante o ID do sitio  
+    app.get('/sitioHidrografiaIDsitio/:id_sitio', getController.sitioHidrografiaIDsitio);
+
+
+    /*-------------------------------Contexto-------------------------------*/
+
+    //Devolve um o contexto de todos os sitios existentes
+     app.get('/sitioContexto', getController.sitioContexto);~
+
+    //Devolve um contexto de um sitio consoante o ID de um contexto
+    app.get('/sitioContexto/:id', getController.sitioContextoID);
+
+    //Devolve um contexto consoante o ID do sitio  
+    app.get('/sitioContextoIDsitio/:id_sitio', getController.sitioContextoIDsitio);
 
 };
